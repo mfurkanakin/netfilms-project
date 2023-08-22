ce8cfd754e8a72a5370fab8dc8f99dc736dd4a2c
furkan akin <furkanakinmfa@gmail.com>
2023-08-23 01:36:09 +0300

    created project and add mocks data

---
+++ src/app/layout.jsx
@@ -0,0 +1,11 @@
+import "../styles/reset.css"
+import "../styles/global.css"
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  )
+}
